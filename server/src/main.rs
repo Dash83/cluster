f33540aac086927f15f6a5c0e20d33ea0188b4ed
Nathan Corbyn <me@nathancorbyn.com>
2019-07-05 16:55:15 +0100

    Fix crash when cloning broken repo

--- server/src/main.rs
+++ server/src/main.rs
@@ -18,107 +18,113 @@ use rocket_contrib::templates::Template;
 use std::fs;
 use std::sync::Mutex;
 
+fn reclone(url: &str, experiment: State<'_, Mutex<Option<Experiment>>>) {
+    fs::remove_dir_all(cluster::PATH).unwrap_or(());
+    match Repository::clone(&url, cluster::PATH) {
+        Ok(_) => {
+            let mut experiment = experiment.lock().unwrap();
+            if let Ok(manifest) = Experiment::load(cluster::experiment_path(), &url) {
+                *experiment = Some(manifest)
+            } else {
+                *experiment = None
+            }
+        }
+        _ => *experiment.lock().unwrap() = None,
+    }
+}
+
 #[get("/")]
-fn index(experiment: State<'_, Mutex<Experiment>>) -> Template {
+fn index(experiment: State<'_, Mutex<Option<Experiment>>>) -> Template {
     Template::render("index", &*experiment.lock().unwrap())
 }
 
 #[get("/ready/<hostname>")]
-fn ready(hostname: String, experiment: State<'_, Mutex<Experiment>>) -> JsonValue {
-    experiment.lock().unwrap().set_running(&hostname, true);
+fn ready(hostname: String, experiment: State<'_, Mutex<Option<Experiment>>>) -> JsonValue {
+    if let Some(ref mut experiment) = *experiment.lock().unwrap() {
+        experiment.set_running(&hostname, true);
+    }
     json!({ "status": "ok" })
 }
 
 #[get("/repo")]
-fn get_repo(experiment: State<'_, Mutex<Experiment>>) -> JsonValue {
-    json!({
-        "status": "ok",
-        "url": experiment.lock().unwrap().url()
-    })
+fn get_repo(experiment: State<'_, Mutex<Option<Experiment>>>) -> JsonValue {
+    if let Some(ref mut experiment) = *experiment.lock().unwrap() {
+        json!({
+            "status": "ok",
+            "url": experiment.url(),
+        })
+    } else {
+        json!({ "status": "err" })
+    }
 }
 
 #[get("/repo/<url>")]
-fn set_repo(
-    url: String,
-    repo: State<'_, Mutex<Repository>>,
-    experiment: State<'_, Mutex<Experiment>>,
-) -> Redirect {
-    fs::remove_dir_all(cluster::PATH).unwrap_or(());
-    match Repository::clone(&url, cluster::PATH) {
-        Ok(cloned) => {
-            *repo.lock().unwrap() = cloned;
-            let mut experiment = experiment.lock().unwrap();
-            *experiment = Experiment::load(cluster::experiment_path(), &url)
-                .expect("failed to parse deployment.toml");
-        }
-        _ => {}
-    }
+fn set_repo(url: String, experiment: State<'_, Mutex<Option<Experiment>>>) -> Redirect {
+    reclone(&url, experiment);
     Redirect::to(uri!(index))
 }
 
 #[get("/update")]
-fn update(
-    repo: State<'_, Mutex<Repository>>,
-    experiment: State<'_, Mutex<Experiment>>,
-) -> Redirect {
-    let mut experiment = experiment.lock().unwrap();
-    fs::remove_dir_all(cluster::PATH).unwrap_or(());
-    *repo.lock().unwrap() =
-        Repository::clone(experiment.url(), cluster::PATH).expect("failed to clone repository");
-    *experiment = Experiment::load(cluster::experiment_path(), experiment.url())
-        .expect("failed to parse deployment.toml");
+fn update(experiment: State<'_, Mutex<Option<Experiment>>>) -> Redirect {
+    let url = if let Some(ref experiment) = *experiment.lock().unwrap() {
+        experiment.url().to_string()
+    } else {
+        return Redirect::to(uri!(index));
+    };
+    reclone(&url, experiment);
     Redirect::to(uri!(index))
 }
 
 #[get("/restart")]
-fn restart(experiment: State<'_, Mutex<Experiment>>) -> Redirect {
-    experiment.lock().unwrap().restart();
+fn restart(experiment: State<'_, Mutex<Option<Experiment>>>) -> Redirect {
+    if let Some(ref mut experiment) = *experiment.lock().unwrap() {
+        experiment.restart();
+    }
     Redirect::to(uri!(index))
 }
 
 #[get("/status")]
-fn status(experiment: State<'_, Mutex<Experiment>>) -> JsonValue {
-    json!({
-        "status": "ok",
-        "hosts": experiment.lock().unwrap().running_hosts(),
-    })
+fn status(experiment: State<'_, Mutex<Option<Experiment>>>) -> JsonValue {
+    if let Some(ref experiment) = *experiment.lock().unwrap() {
+        json!({
+            "status": "ok",
+            "hosts": experiment.running_hosts(),
+        })
+    } else {
+        json!({ "status": "err" })
+    }
 }
 
 #[get("/status/<hostname>")]
-fn host_status(hostname: String, experiment: State<'_, Mutex<Experiment>>) -> JsonValue {
-    let experiment = experiment.lock().unwrap();
-    if let Some(host) = experiment.get(&hostname) {
-        json!({
-            "status": "ok",
-            "url": experiment.url(),
-            "host": hostname,
-            "running": host.running().to_string(),
-            "restarted": experiment.restarted().to_string(),
-        })
+fn host_status(hostname: String, experiment: State<'_, Mutex<Option<Experiment>>>) -> JsonValue {
+    if let Some(ref experiment) = *experiment.lock().unwrap() {
+        if let Some(host) = experiment.get(&hostname) {
+            json!({
+                "status": "ok",
+                "url": experiment.url(),
+                "host": hostname,
+                "running": host.running().to_string(),
+                "restarted": experiment.restarted().to_string(),
+            })
+        } else {
+            json!({
+                "status": "ok",
+                "url": experiment.url(),
+                "host": hostname,
+                "restarted": experiment.restarted().to_string(),
+            })
+        }
     } else {
         json!({
             "status": "ok",
-            "url": experiment.url(),
             "host": hostname,
-            "restarted": experiment.restarted().to_string(),
         })
     }
 }
 
-fn init() -> (Repository, Experiment) {
-    fs::remove_dir_all(cluster::PATH).unwrap_or(());
-    let repo =
-        Repository::clone(cluster::DEFAULT, cluster::PATH).expect("failed to clone repository");
-    let experiment = Experiment::load(cluster::experiment_path(), cluster::DEFAULT)
-        .expect("failed to parse deployment.toml");
-    (repo, experiment)
-}
-
 fn main() {
-    let (repo, experiment) = init();
     rocket::ignite()
-        .manage(Mutex::new(repo))
-        .manage(Mutex::new(experiment))
+        .manage::<Mutex<Option<Experiment>>>(Mutex::new(None))
         .mount("/", routes![index])
         .mount(
             "/api/",
